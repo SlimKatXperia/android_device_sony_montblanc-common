ada6fe9f572ccb2b5d2693b2994e96b87ad0257c
speedbot <x95a31xreg@gmail.com>
2014-05-19 17:36:08 +0200

    Revert "We optimize the code in Build Repo"
    This reverts commit efd5bb8e5abb07c8e030b5f17c6748cda0caf55a.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,6 +15,8 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a9
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
