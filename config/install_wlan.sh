42df7a11989de57b25915d5f213ce26c7d5e3bd8
95A31 <x95a31xreg@gmail.com>
2014-09-02 15:21:35 +0200

    No p2p0 interface

--- config/install_wlan.sh
+++ config/install_wlan.sh
@@ -1,9 +1,5 @@
 #!/system/bin/sh
 
 # Load Wireless modules
-/system/bin/insmod /system/lib/modules/cw1200_core.ko macaddr=$(/system/bin/cat /data/etc/wlan_macaddr)
+/system/bin/insmod /system/lib/modules/cw1200_core.ko macaddr=$(/sbin/cat /data/etc/wlan_macaddr)
 /system/bin/insmod /system/lib/modules/cw1200_wlan.ko
-
-# Create p2p0 interface
-/system/bin/iw phy phy0 interface add p2p0 type managed
-ifconfig p2p0 hw ether $(hexdump -n6 -e '1/1 ":%02X"' /dev/random | tail -c 17)
